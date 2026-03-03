970fc38f42e5e76b83fffef2993fc3d8577aa893
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-03-03 21:44:38 +0100

    feat: create BookController with Get all books endpoint

---
+++ src/main/java/com/adem/book_library_api/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.adem.book_library_api.controller;
+
+import com.adem.book_library_api.model.Book;
+import com.adem.book_library_api.repository.BookRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    // Injection par constructeur (L'équivalent du typage dans le constructeur Laravel)
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
